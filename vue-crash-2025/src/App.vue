bad2e99634bb97f994e9717f5a9249a283626272
lovindy <seangdy085@gmail.com>
2024-11-23 21:47:16 -0800

    initial project

---
+++ vue-crash-2025/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "SeangDev",
+      status: "pending",
+      tasks: ["Task One", "Task Two", "Task Three"],
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <!-- Interpolation in Vue.js -->
+    <p>Hello Vue.js from {{ name }}</p>
+
+    <!-- Conditional rendering with v-show -->
+    <p v-if="status === 'active'">SeangDev is Active</p>
+    <p v-else-if="status === 'pending'">SeangDev is Pending</p>
+    <p v-else>SeangDev is inactive</p>
+
+    <!-- Looping through an array -->
+    <ul>
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
+  </div>
+</template>
